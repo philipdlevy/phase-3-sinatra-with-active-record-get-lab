b2a23a06166a8807d0ced8ed51c0b2d1fa988db8
Philip Levy <philipdlevy@gmail.com>
2022-06-13 17:53:54 -0600

    commit tree c56750dd70ca1e8d11285b5e007e4888acf75997

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
 
+  
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_good_order = BakedGood.all
+    baked_good_order.order(price: :desc).to_json
+  end 
+
+  get '/baked_goods/most_expensive' do
+    # binding.pry
+    baked_goods = BakedGood.all
+    baked_goods.order(price: :desc).first.to_json
+  end
 end
